d4fff31e685f3ef2740eac4fb38eab3f51d94777
Matthew Newman <matt.newman@gmail.com>
2020-06-15 21:01:13 -0500

    Passport Facebook Server Issue with Index

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '251285712833967',
+        clientSecret: '860e1f41b985ef6de431a912b956b016'
+    }
 }
