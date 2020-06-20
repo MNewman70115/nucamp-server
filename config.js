c45cf8a96ab9775af2c4ae8a1dd48eaa8b1a44ca
Matthew Newman <matt.newman@gmail.com>
2020-06-20 18:19:45 -0500

    I added to gitignore but wasn’t ignored

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
-    'facebook': {
-        clientId: '251285712833967',
-        clientSecret: '860e1f41b985ef6de431a912b956b016'
-    }
-}
